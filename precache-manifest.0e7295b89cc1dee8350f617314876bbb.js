accd3031b0590a770d26ad33169684ef60df3502
Floyd Jones <floydtjones@gmail.com>
2019-09-29 22:14:28 -0500

    Updates

---
+++ precache-manifest.0e7295b89cc1dee8350f617314876bbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407da0ad0a120eb7208fc561936154bb",
+    "url": "/farmers-market/index.html"
+  },
+  {
+    "revision": "f22f33e0800859dc056d",
+    "url": "/farmers-market/static/css/main.595e30da.chunk.css"
+  },
+  {
+    "revision": "f02d2120f0ec7a6d1f08",
+    "url": "/farmers-market/static/js/2.d47b1626.chunk.js"
+  },
+  {
+    "revision": "f22f33e0800859dc056d",
+    "url": "/farmers-market/static/js/main.ee9a4c06.chunk.js"
+  },
+  {
+    "revision": "e81a5b9b6a7ee327bb8d",
+    "url": "/farmers-market/static/js/runtime-main.ff92d87f.js"
+  }
+]);
